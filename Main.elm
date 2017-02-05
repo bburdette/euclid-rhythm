69a954c3ad486ca8e7e0d741762deacca716f749
bburdette <bburdette@gmail.com>
2017-02-05 11:26:38 -0700

    add descriptions; center everything; 'try X and . keys'

--- Main.elm
+++ Main.elm
@@ -9,6 +9,7 @@ import Html exposing (Html, Attribute, text, div, input, span, button)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick)
 import Tuple exposing (first, second)
+import Dict
 import String
 
 
@@ -142,18 +143,16 @@ onKeyUp tagger =
 --  Debug.log (toString i) (Key i)
 
 view model =
-  div [ ]
+  div [ style [ ("text-align", "center") ] ]
     [ span [] [ text "ones"
               , button [onClick OneUp] [text "moar"] 
               , text (toString model.ones)
               , button [onClick OneDown] [text "less"] 
-             -- , input [ placeholder "Text to reverse", onInput Ones, myStyle ] []
               ]
     , span [] [ text "zeros"
               , button [onClick ZeroUp] [text "moar"] 
               , text (toString model.zeros)
               , button [onClick ZeroDown] [text "less"] 
-             -- , input [ placeholder "Text to reverse", onInput Ones, myStyle ] []
               ]
     , div [ myStyle ] [pattwbold (showpattern (bjorkland model.ones model.zeros)) model.pos]
     , div [] [ button [onClick XClick] [text "x"]
@@ -162,6 +161,8 @@ view model =
              , text (toString model.correctCount)]
     , div [] [ text "incorrect: "
              , text (toString model.incorrectCount)]
+    , div [] [ text "try X and . keys" ]
+    , span [] [ text (Maybe.withDefault "" (Dict.get (model.ones, (model.ones + model.zeros)) descriptionDict)) ]
     ]
 
 -- pattwbold: String -> Int -> 
@@ -188,3 +189,32 @@ notMyStyle =
     , ("text-align", "center")
     ]
 
+---------------------------------------------------------------------------
+-- rhythm descriptions from the BANFF paper.
+
+descriptions : List ((Int,Int), String)
+descriptions =  [((4,12), "This is the (12/8)-time Fandango clapping pattern in the Flamenco music of southern Spain, where 'x' denotes a loud clap and '.' a soft clap [10]."),
+        ((2,3), "This is a common Afro-Cuban drum pattern. For example, it is the conga rhythm of the (6/8)-time Swing Tumbao [18]. It is also common in Latin American music, as for example in the Cueca."),
+        ((2,5), "This is a thirteenth century Persian rhythm called Khafif-e-ramal. It is also the metric pattern of the second movement of Tchaikovsky's Symphony No. 6. When it is started on the second onset ([x . . x .]) it is the metric pattern of Dave Brubeck's Take Five as well as Mars from The Planets by Gustav Holst."),
+        ((3,4), "This is the archetypal pattern of the Cumbia from Colombia [20], as well as a Calypso rhythm from Trinidad [13]. It is also a thirteenth century Persian rhythm called Khalif-e-saghil [34], as well as the trochoid choreic rhythmic pattern of ancient Greece [21]."),
+        ((3,5), "when started on the second onset, is another thirteenth century Persian rhythm by the name of Khafif-e-ramal [34], as well as a Rumanian folk-dance rhythm [25]."),
+        ((3,7), "This is a Ruchenitza rhythm used in a Bulgarian folk-dance [24]. It is also the metric pattern of Pink Floyd's Money [17]."),
+        ((3,8), "This is the Cuban tresillo pattern discussed in the preceding [15]."),
+        ((4,7), "This is another Ruchenitza Bulgarian folk-dance rhythm [24]."),
+        ((4,9), "This is the Aksak rhythm of Turkey [6]. It is also the metric pattern used by Dave Brubeck in his piece Rondo a la Turk [17]."),
+        ((4,11), "This is the metric pattern used by Frank Zappa in his piece titled Outside Now [17]."),
+        ((5,6), "yields the York-Samai pattern, a popular Arab rhythm, when started on the second onset [30]."),
+        ((5,7), "This is the Nawakhat pattern, another popular Arab rhythm [30]."),
+        ((5,8), "This is the Cuban cinquillo pattern discussed in the preceding [15]. When it is started on the second onset it is also the Spanish Tango [13] and a thirteenth century Persian rhythm, the Al-saghilal-sani [34]."),
+        ((5,9), "This is a popular Arab rhythm called Agsag-Samai [30]. When started on the second onset, it is a drum pattern used by the Venda in South Africa [26], as well as a Rumanian folk-dance rhythm [25]."),
+        ((5,11), "This is the metric pattern used by Moussorgsky in Pictures at an Exhibition [17]."),
+        ((5,12), "This is the Venda clapping pattern of a South African children's song [24]."),
+        ((5,16), "This is the Bossa-Nova rhythm necklace of Brazil. The actual Bossa-Nova rhythm usually starts on the third onset as follows: [x . . x . . x . . . x . . x . .] [31]. However, there are other starting places as well, as for example [x . . x . . x . . x . . . x . .] [3]."),
+        ((7,8), "This is a typical rhythm played on the Bendir (frame drum), and used in the accompaniment of songs of the Tuareg people of Libya [30]."),
+        ((7,12), "This is a commonWest African bell pattern. For example, it is used in the Mpre rhythm of the Ashanti people of Ghana [32]."),
+        ((7,16), "This is a Samba rhythm necklace from Brazil. The actual Samba rhythm is [x . x . . x . x . x . . x . x .] obtained by starting E(7,16) on the last onset. When E(7,16) is started on the fifth onset it is a clapping pattern from Ghana [24]."),
+        ((9,16), "This is a rhythm necklace used in the Central African Republic [2].  When it is started on the fourth onset it is a rhythm played in West and Central Africa [15],, as well as a cow-bell pattern in the Brazilian samba [29]. When it is started on the penultimate onset it is the bell pattern of the Ngbaka-Maibo rhythms of the Central African Republic [2]."),
+        ((11,24), "This is a rhythm necklace of the Aka Pygmies of Central Africa [2]. It is usually started on the seventh onset."),
+        ((13,24), "This is another rhythm necklace of the Aka Pygmies of the upper Sangha [2]. It is usually started on the fourth onset.")]
+
+descriptionDict = Dict.fromList descriptions
